db0803d186a214b6083941061bc54158423e0eb7
Mateusz Front <mateusz.front@swmansion.com>
2025-06-04 17:11:42 +0200

    refactor

--- lib/async_test.ex
+++ lib/async_test.ex
@@ -6,111 +6,102 @@ defmodule AsyncTest do
   # - copies all @tags to the newly created module
   # - setup and setup_all won't work (yet)
 
-  defmacro async_test(
-             test_name,
-             context \\ quote do
-               %{}
-             end,
-             do: block
-           ) do
+  defmacrop unquoted(code) do
+    quoted = {:quote, [], [[do: code]]}
+    unquoted = [:unquote, [], [quoted]]
+    escaped_once = {:{}, [], unquoted}
+    escaped_once
+  end
+
+  defmacro async_test(test_name, context \\ quote(do: %{}), do: block) do
     quote do
-      id = unquote(test_name)
-      test_module_name = Module.concat(__MODULE__, "AsyncTest_#{id}")
-      fun_name = :"async_test_#{id}"
-      after_compile_fun_name = :"async_test_ac_#{id}"
+      params = unquote(__MODULE__).__params__(unquote(test_name), __MODULE__)
 
-      tags_attrs =
-        [:tag, :describetag, :moduletag]
-        |> Enum.flat_map(fn attr ->
-          Module.get_attribute(__MODULE__, attr)
-          |> Enum.map(&{attr, &1})
-        end)
-
-      setups =
-        Module.get_attribute(__MODULE__, :ex_unit_setup)
-        |> Enum.map(fn
-          {module, fun} -> {module, fun}
-          fun -> {__MODULE__, :"__async_test_setup_#{fun}"}
-        end)
-
-      Module.get_attribute(__MODULE__, :ex_unit_setup)
-      |> Enum.each(fn
-        {_module, _fun} ->
-          nil
-
-        fun ->
-          name = :"__async_test_setup_#{fun}"
-
-          def unquote(unquoted_var(:name))(ctx) do
-            unquote(unquoted_var(:fun))(ctx)
-          end
+      Enum.each(params.setups.proxies, fn %{proxy: proxy, fun: fun} ->
+        def unquote(unquoted(proxy))(ctx) do
+          unquote(unquoted(fun))(ctx)
+        end
       end)
 
-      setup_alls =
-        Module.get_attribute(__MODULE__, :ex_unit_setup_all)
-        |> Enum.map(fn
-          {module, fun} -> {module, fun}
-          fun -> {__MODULE__, :"__async_test_setup_all_#{fun}"}
-        end)
-
-      Module.get_attribute(__MODULE__, :ex_unit_setup_all)
-      |> Enum.each(fn
-        {_module, _fun} ->
-          nil
-
-        fun ->
-          name = :"__async_test_setup_all_#{fun}"
-
-          unless Module.defines?(__MODULE__, {name, 1}) do
-            def unquote(unquoted_var(:name))(ctx) do
-              agent_name = Module.concat(__MODULE__, unquote(unquoted_var(:name)))
-              Agent.start_link(fn -> unquote(unquoted_var(:fun))(ctx) end, name: agent_name)
-              Agent.get(agent_name, & &1)
-            end
-          end
+      Enum.each(params.setup_alls.proxies, fn %{proxy: proxy, fun: fun} ->
+        def unquote(unquoted(proxy))(ctx) do
+          agent_name = Module.concat(__MODULE__, unquote(unquoted(proxy)))
+          Agent.start_link(fn -> unquote(unquoted(fun))(ctx) end, name: agent_name)
+          Agent.get(agent_name, & &1)
+        end
       end)
 
-      def unquote(unquoted_var(:fun_name))(unquote(context)) do
+      def unquote(unquoted(params.fun_name))(unquote(context)) do
         unquote(block)
       end
 
-      def unquote(unquoted_var(:after_compile_fun_name))(_bytecode, _env) do
-        test_name = unquote(unquoted(test_name))
-        fun_name = unquote(unquoted_var(:fun_name))
-        tags_attrs = unquote(unquoted_var(:tags_attrs))
-        setups = unquote(unquoted_var(:setups))
-        setup_alls = unquote(unquoted_var(:setup_alls))
+      escaped_params = Macro.escape(params)
 
-        content =
-          quote do
-            use ExUnit.Case, async: true
-
-            Enum.each(unquote(tags_attrs), fn {name, value} ->
-              Module.put_attribute(__MODULE__, name, value)
-            end)
-
-            @ex_unit_setup unquote(setups)
-            @ex_unit_setup_all unquote(setup_alls)
-
-            test unquote(test_name), context do
-              unquote(__MODULE__).unquote(fun_name)(context)
-            end
-          end
-
-        Module.create(unquote(unquoted_var(:test_module_name)), content, __ENV__)
+      def unquote(unquoted(params.after_compile_fun_name))(_bytecode, _env) do
+        params = unquote(unquoted(escaped_params))
+        unquote(__MODULE__).__create_module__(__MODULE__, params)
       end
 
-      @after_compile {__MODULE__, after_compile_fun_name}
+      @after_compile {__MODULE__, params.after_compile_fun_name}
 
       Module.delete_attribute(__MODULE__, :tag)
     end
   end
 
-  defp unquoted_var(name) do
-    unquoted(Macro.var(name, __MODULE__))
+  def __params__(test_name, module) do
+    tags_attrs =
+      [:tag, :describetag, :moduletag]
+      |> Enum.flat_map(fn attr ->
+        Module.get_attribute(module, attr) |> Enum.map(&{attr, &1})
+      end)
+
+    %{
+      test_name: test_name,
+      test_module_name: Module.concat(module, "AsyncTest_#{test_name}"),
+      fun_name: :"async_test_#{test_name}",
+      after_compile_fun_name: :"async_test_ac_#{test_name}",
+      setups: setups_proxies(module, :ex_unit_setup),
+      setup_alls: setups_proxies(module, :ex_unit_setup_all),
+      tags_attrs: tags_attrs
+    }
   end
 
-  defp unquoted(ast) do
-    {:unquote, [], [ast]}
+  def __create_module__(caller_module, params) do
+    content =
+      quote do
+        use ExUnit.Case, async: true
+
+        Enum.each(unquote(params.tags_attrs), fn {name, value} ->
+          Module.put_attribute(__MODULE__, name, value)
+        end)
+
+        @ex_unit_setup unquote(params.setups.proxied_attr)
+        @ex_unit_setup_all unquote(params.setup_alls.proxied_attr)
+
+        test unquote(params.test_name), context do
+          unquote(caller_module).unquote(params.fun_name)(context)
+        end
+      end
+
+    Module.create(params.test_module_name, content, __ENV__)
+  end
+
+  defp setups_proxies(module, attr_name) do
+    attr = Module.get_attribute(module, attr_name)
+
+    proxied_attr =
+      Enum.map(attr, fn
+        {module, fun} -> {module, fun}
+        fun -> {module, :"__async_test_#{attr_name}#{fun}"}
+      end)
+
+    proxies =
+      Enum.flat_map(attr, fn
+        {_module, _fun} -> []
+        fun -> [%{fun: fun, proxy: :"__async_test_#{attr_name}#{fun}"}]
+      end)
+      |> Enum.reject(&Module.defines?(module, {&1.proxy, 1}))
+
+    %{proxied_attr: proxied_attr, proxies: proxies}
   end
 end
